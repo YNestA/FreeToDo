de0e99efa497a3d2c21a0f9954e0165b801259b7
YNestA <U201417256@hust.edu.cn>
2017-03-01 22:57:28 +0800

    3.1

---
+++ static/js/login.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    var LRVM=new Vue({
+        el:"#main",
+        delimiters:["[[","]]"],
+        data:{
+            logining:true,
+        },
+    });
+});
---
+++ static/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    var menuVM=new Vue({
+        el:"#whole-menu",
+        delimiters:["[[","]]"],
+        data:{
+
+        },
+    });
+});
